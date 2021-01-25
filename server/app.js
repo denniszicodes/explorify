12a749e7ff268fe013a980a46e5f84add3fa9d5e
Dennis Ziemer <37363429+denniszicodes@users.noreply.github.com>
2021-01-25 17:19:25 +0100

    updated refresh_token api (#3)
    Co-authored-by: denniszicodes <denniszicodes@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -9,14 +9,18 @@ const axios = require("axios");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const CLIENT_ID = process.env.CLIENT_ID || null;
+const CLIENT_SECRET = process.env.CLIENT_SECRET || null;
 const PORT = process.env.PORT || 8000;
-const REDIRECT_URI =
-  process.env.REDIRECT_URI || "http://localhost:8080/callback";
-const FRONTEND_URI = process.env.FRONTEND_URI;
+let REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:8080/callback";
+let FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:3000";
 
 const authStateKey = "spotify_auth_state";
+
+if (process.env.NODE_ENV !== "production") {
+  REDIRECT_URI = "http://localhost:8080/callback";
+  FRONTEND_URI = "http://localhost:3000";
+}
 
 const app = express();
 
@@ -26,17 +30,17 @@ app.use(cors());
 
 app.get("/login", (req, res, next) => {
   const state = uuid();
-  const scopes = "user-read-private user-read-email user-read-recently-played";
+  const scope = "user-read-private user-read-email";
 
   res.cookie(authStateKey, state);
 
-  res.redirect(
+  res.status(302).redirect(
     `https://accounts.spotify.com/authorize?${querystring.stringify({
       response_type: "code",
       client_id: CLIENT_ID,
       redirect_uri: REDIRECT_URI,
       state: state,
-      scope: scopes,
+      scope: scope,
     })}`
   );
 });
@@ -45,8 +49,13 @@ app.get("/callback", (req, res, next) => {
   const code = req.query.code;
   const state = req.query.state;
   const storedState = req.cookies ? req.cookies[authStateKey] : null;
+
   if (storedState === null || storedState !== state) {
-    throw new Error("invalid");
+    res.status(302).redirect(
+      `/#${querystring.stringify({
+        error: "invalid_state",
+      })}`
+    );
   } else {
     res.clearCookie(authStateKey);
 
@@ -79,8 +88,46 @@ app.get("/callback", (req, res, next) => {
           })}`
         );
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        res
+          .status(302)
+          .redirect(`/#${querystring.stringify({ error: "invalid_token" })}`);
+      });
   }
 });
 
-app.listen(PORT);
+app.get("/refresh-token", (req, res, next) => {
+  const refresh_token = req.query.refresh_token || null;
+
+  if (!refresh_token) {
+    throw new Error("Invalid request. No refresh_token supplied.");
+  }
+
+  const data = {
+    grant_type: "refresh_token",
+    refresh_token,
+  };
+
+  axios
+    .post(
+      `https://accounts.spotify.com/api/token`,
+      querystring.stringify(data),
+      {
+        headers: {
+          Authorization: `Basic ${new Buffer.from(
+            `${CLIENT_ID}:${CLIENT_SECRET}`
+          ).toString("base64")}`,
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+    .then((response) => {
+      const { access_token } = response.data;
+      res.status(200).json({ access_token });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port: ${PORT}`);
+});
