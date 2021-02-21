12fd9d1d4a265f156c9438b0286bedfac5b84dee
Dennis Ziemer <37363429+denniszicodes@users.noreply.github.com>
2021-02-21 15:56:33 +0100

    21 clean up auth file and comments (#8)
    * fix request-token route
    
    - added request parameter in url "refresh_token=" in auth file
    
    * improvements on auth file with clean ups
    
    - added comments
    - improve refreshAccessToken function and logout function
    - remove unnecessary codeblocks
    
    Co-authored-by: denniszicodes <denniszicodes@users.noreply.github.com>
    Co-authored-by: Christoph Lindstädt <45950730+chrislicodes@users.noreply.github.com>

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -1,41 +1,35 @@
 import axios from "axios";
 import { getHashParams } from "../utils/utils";
 
-//------------------------------------------------
-//---------> TOKENS
-//------------------------------------------------
-
-//SET Methods for Localstorage Keys
-const setLocalTokenExpiresIn = () =>
-  localStorage.setItem("spotify_expires_in", getHashParams().expires_in);
-
-const setLocalTokenTimestamp = () =>
-  localStorage.setItem("spotify_token_timestamp", Date.now());
-
-const setLocalAccessToken = () => {
+/**
+ * SET methods for localstorage keys
+ */
+const setLocalTokenExpiresIn = (expires_in) => localStorage.setItem("spotify_expires_in", expires_in);
+const setLocalTokenTimestamp = () => localStorage.setItem("spotify_token_timestamp", Date.now());
+const setLocalAccessToken = (access_token) => {
   setLocalTokenTimestamp();
-  localStorage.setItem("spotify_access_token", getHashParams().access_token);
+  localStorage.setItem("spotify_access_token", access_token);
 };
+const setLocalRefreshToken = (refresh_token) => localStorage.setItem("spotify_refresh_token", refresh_token);
 
-const setLocalRefreshToken = () =>
-  localStorage.setItem("spotify_refresh_token", getHashParams().refresh_token);
-
-//GET Methods for Localstorage Keys
+/**
+ * GET methods for localstorage keys
+ */
 const getLocalTokenExpiresIn = () => localStorage.getItem("spotify_expires_in");
-const getLocalTokenTimestamp = () =>
-  localStorage.getItem("spotify_token_timestamp");
+const getLocalTokenTimestamp = () => localStorage.getItem("spotify_token_timestamp");
 const getLocalAccessToken = () => localStorage.getItem("spotify_access_token");
-const getLocalRefreshToken = () =>
-  localStorage.getItem("spotify_refresh_token");
+const getLocalRefreshToken = () => localStorage.getItem("spotify_refresh_token");
 
+/**
+ * Refresh the access_token if serveral validations in function "getAccessToken" fails
+ */
 const refreshAccessToken = () => {
   axios
-    .get(
-      `http://localhost:8080/refresh-token?refresh_token=${getLocalRefreshToken()}`
-    )
+    .get(`http://localhost:8080/refresh-token?refresh_token=${getLocalRefreshToken()}`)
     .then(({ data }) => {
       const { access_token } = data;
       setLocalAccessToken(access_token);
+      window.location.reload();
       return;
     })
     .catch((err) => {
@@ -43,36 +37,43 @@ const refreshAccessToken = () => {
     });
 };
 
+/**
+ * Returns access_token for the application to detect if user has a access_token -> is logged in
+ * Checks for several condition and probaly refresh the access_token
+ *
+ * @returns {string || undefined} access_token
+ */
 export const getAccessToken = () => {
   const { error, access_token, refresh_token, expires_in } = getHashParams();
 
+  // If error exist in getHashParams then refresh access_token
   if (error) {
     refreshAccessToken();
   }
 
-  const localTokenExpiresIn = getLocalTokenExpiresIn();
+  const localTokenExpiresIn = +getLocalTokenExpiresIn();
   const localAccessToken = getLocalAccessToken();
   const localRefreshToken = getLocalRefreshToken();
 
-  //If no expires_in is in localstoragem then set it from params
+  // If no expires_in is in localstoragem then set it from getHashParams
   if (!localTokenExpiresIn || localTokenExpiresIn === "undefined") {
     setLocalTokenExpiresIn(expires_in);
   }
 
-  const EXPIRATION_TIME = getLocalTokenExpiresIn() * 1000; // 1 hour in milliseconds -> 3600 secounds * 1000 -> 3600 Spotify default
+  const EXPIRATION_TIME = localTokenExpiresIn * 1000; // 1 hour in milliseconds -> 3600 secounds * 1000 -> 3600 Spotify default
 
-  //Check if token has expired
+  // If access_token is expired -> get new access_token with refresh_token
   if (Date.now() - getLocalTokenTimestamp() > EXPIRATION_TIME) {
     console.warn("Access token has expired, refreshing token ...");
     refreshAccessToken();
   }
 
-  //If no refresh_token in localstorage, then set it from params
+  // If no refresh_token in localstorage, then set it from getHashParams
   if (!localRefreshToken || localRefreshToken === "undefined") {
     setLocalRefreshToken(refresh_token);
   }
 
-  //If no access_token in localstorage, then set it from params
+  // If no access_token in localstorage, then set it from getHashParams
   if (!localAccessToken || localAccessToken === "undefined") {
     setLocalAccessToken(access_token);
     return access_token;
@@ -81,10 +82,14 @@ export const getAccessToken = () => {
   return localAccessToken;
 };
 
-//Method to logout user from application
+/**
+ * Logout function to logout the user from the application
+ * Removes all important localstorage keys and reload the application
+ */
 export const logout = () => {
   localStorage.removeItem("spotify_expires_in");
   localStorage.removeItem("spotify_token_timestamp");
   localStorage.removeItem("spotify_access_token");
   localStorage.removeItem("spotify_refresh_token");
+  window.location.reload("");
 };
