e91e03c1130ca45eaa33deea7718cd43cfa2c893
Christoph Lindstädt <45950730+chrislicodes@users.noreply.github.com>
2021-02-22 23:50:27 +0100

    12 add logo and title to sidenav (#12)
    * added svg logo and created apptitle component
    
    * adjusted margin
    
    * fixed link scope
    
    Co-authored-by: Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>

---
+++ client/src/layouts/Explorify/SideNav/AppTitle/AppTitle.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SpotifyIcon from "./SpotifyIcon/SpotifyIcon";
+import classes from "./AppTitle.module.css";
+import { Link } from "react-router-dom";
+
+const AppTitle = () => {
+  return (
+    <div>
+      <Link to="/" className={classes.AppTitle}>
+        <SpotifyIcon />
+        <h1>Explorify</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default AppTitle;
--- client/src/layouts/Explorify/SideNav/SideNav.js
+++ client/src/layouts/Explorify/SideNav/SideNav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./SideNav.module.css";
 import NavItem from "./NavItem/NavItem";
+import AppTitle from "./AppTitle/AppTitle";
 
 const Header = () => {
   return (
     <nav className={classes.SideNav}>
+      <AppTitle />
       <ul>
         <NavItem target="Analyze" />
       </ul>
