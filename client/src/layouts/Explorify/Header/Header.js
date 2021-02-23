82d690747f0b0c1c89cadb4fe2e37a3e3fceabc4
Dennis Ziemer <37363429+denniszicodes@users.noreply.github.com>
2021-02-23 15:31:00 +0100

    create logout button (#13)
    * create logout button
    
    * fixed color variables
    
    * added blank line at the end of file
    
    Co-authored-by: denniszicodes <denniszicodes@users.noreply.github.com>
    Co-authored-by: Christoph Lindstädt <45950730+chrislicodes@users.noreply.github.com>

--- client/src/layouts/Explorify/Header/Header.js
+++ client/src/layouts/Explorify/Header/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
 import classes from "./Header.module.css";
+import { logout } from "../../../auth/auth";
 
 const Header = () => {
-  return <header className={classes.Header}>Header</header>;
+  return (
+    <header className={classes.Header}>
+      Header
+      <button className={classes.LogoutBtn} onClick={logout}>
+        LOGOUT
+      </button>
+    </header>
+  );
 };
 
 export default Header;
