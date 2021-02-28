c8eddc4b7285b9ecdd274bc4a95f6ddd8e7bda42
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-02-28 18:50:32 +0100

    header fix

--- client/src/layouts/Explorify/Header/Header.js
+++ client/src/layouts/Explorify/Header/Header.js
@@ -5,21 +5,16 @@ import { logout } from "../../../auth/auth";
 import SearchIcon from "./components/SearchIcon/SearchIcon";
 import { AuthContext } from "../../../context/AuthContext";
 
-
 const Header = () => {
   let { display_name: username, images: userimage } = useContext(AuthContext);
 
   return (
     <header className={classes.Header}>
-
       <div className={classes.contentWrapper}>
         <SearchIcon />
-        <button className={classes.LogoutBtn} onClick={logout}>
-          LOGOUT
-        </button>
-      
-        <img src={userimage[0].url} className={classes.Userimage} alt="user" />
-        <p className={classes.Username}>{username}</p>
+
+        <img src={userimage[0].url} className={classes.userimage} alt="user" />
+        <p className={classes.username}>{username}</p>
         <button className={classes.LogoutBtn} onClick={logout}>
           LOGOUT
         </button>
